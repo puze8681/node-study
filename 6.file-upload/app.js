add9c77ca3634974aa5f26bd5a95c56219625f90
puze8681 <rsef0601@naver.com>
2017-10-04 20:34:06 +0900

    now

--- 6.file-upload/app.js
+++ 6.file-upload/app.js
@@ -1,3 +1,7 @@
 const exrpess = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const multer = require('multer');
+const fs = require('fs')
+const app = express();
+const schema = mongoose.Schema;
