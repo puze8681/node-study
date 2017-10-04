f52c0fedd09b1a9a013386e84159ba71ba8fe0fa
puze8681 <rsef0601@naver.com>
2017-10-04 21:00:46 +0900

    file upload

--- 6.file-upload/app.js
+++ 6.file-upload/app.js
@@ -1,7 +1,43 @@
-const exrpess = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const multer = require('multer');
-const fs = require('fs')
-const app = express();
-const schema = mongoose.Schema;
+var exrpess = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var multer = require('multer');
+var fs = require('fs')
+var app = express();
+var schema = mongoose.Schema;
+var stroage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null, '/home/harddisk/harddisk')
+    },
+    filename: (req,fidle,cb)=>{
+        cb(null, file.originalname)
+    }
+})
+var upload = multer({storage: storage})
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use('/file', express.static('file'))
+
+app.listen(3000, ()=>{
+    console.log('Server Running At 3000 Port!')
+})
+
+app.get('/', (req,res)=>{
+    fs.readFile('index.html', 'utf-8', (err,data)=>{
+        res.send(data)
+    })
+})
+
+app.get('/movieupload', (req,res)=>{
+    fs.readFile('movie.html', 'utf-8', (err,data)=>{
+        res.send(data)
+    })
+})
+
+app.post('/moviewupload', upload.single('file'), (req,res)=>{
+    console.log(req.file)
+    res.send('Success')
+})
